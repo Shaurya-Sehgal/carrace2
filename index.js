4a5838cbbde02aa95faeba0ad5ccd4a5156112f6
Shaurya-Sehgal <102002567+Shaurya-Sehgal@users.noreply.github.com>
2022-05-16 20:30:38 -0600

    Update index.js
    Made it so player1/player2 can't click on their button twice in a row.

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
+let checkAnswerExecuted = false
 let position = 1
 let position2 = 20
 function moveCar1(){
-    let speed = Math.random()*10
-    speed = Math.floor(speed)%3 + 1
-    document.getElementById(position).src = "https://www.roadsbridges.com/sites/rb/files/styles/content_type_page/public/Traffic_Safety_64.jpg?itok=a9CICO90"
-    position = position + speed
-    if(position > 9){
-        location.href = "win.html"
-    }
-    document.getElementById(position).src = "https://static.vecteezy.com/system/resources/previews/001/193/929/large_2x/vintage-car-png.png"
-
+    if(checkAnswerExecuted == false){
+        let speed = Math.random()*10
+        speed = Math.floor(speed)%3 + 1
+        document.getElementById(position).src = "https://www.roadsbridges.com/sites/rb/files/styles/content_type_page/public/Traffic_Safety_64.jpg?itok=a9CICO90"
+        position = position + speed
+        if(position > 9){
+            location.href = "win.html"
+        }
+}
+document.getElementById(position).src = "https://static.vecteezy.com/system/resources/previews/001/193/929/large_2x/vintage-car-png.png"
+checkAnswerExecuted = true
 }
 function moveCar2(){
-    let speed2 = Math.random()*10
-    speed2 = Math.floor(speed2)%3 + 1
-    document.getElementById(position2).src = "https://www.roadsbridges.com/sites/rb/files/styles/content_type_page/public/Traffic_Safety_64.jpg?itok=a9CICO90"
-    position2 = position2 + speed2
+    if(checkAnswerExecuted == true){
+        let speed2 = Math.random()*10
+        speed2 = Math.floor(speed2)%3 + 1
+        document.getElementById(position2).src = "https://www.roadsbridges.com/sites/rb/files/styles/content_type_page/public/Traffic_Safety_64.jpg?itok=a9CICO90"
+        position2 = position2 + speed2
+    }
     if(position2 > 28){
         location.href = "win2.html"
     }
     document.getElementById(position2).src = "https://static.vecteezy.com/system/resources/previews/001/193/929/large_2x/vintage-car-png.png"
+    checkAnswerExecuted = false
 }
