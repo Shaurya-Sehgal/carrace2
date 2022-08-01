1e05b47a6f67bd6a9a62abb487f7922f015e8cf6
Shaurya-Sehgal <ashish.shaurya.as@gmail.com>
2022-07-31 21:06:16 -0600

    Fixed road glitch

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
-let checkAnswerExecuted = false
-let position = 1
-let position2 = 20
-function moveCar1(){
-    if(checkAnswerExecuted == false){
-        let speed = Math.random()*10
-        speed = Math.floor(speed)%3 + 1
-        document.getElementById(position).src = "https://www.roadsbridges.com/sites/rb/files/styles/content_type_page/public/Traffic_Safety_64.jpg?itok=a9CICO90"
-        position = position + speed
-    }
-    else{
-        alert("It's The Other Players Turn!")
-    }
-        if(position > 9){
-            location.href = "win.html"
-        }
-document.getElementById(position).src = "https://static.vecteezy.com/system/resources/previews/001/193/929/large_2x/vintage-car-png.png"
-checkAnswerExecuted = true
+let checkAnswerExecuted = false;
+let position = 1;
+let position2 = 20;
+function moveCar1() {
+  if (checkAnswerExecuted == false) {
+    let speed = Math.random() * 10;
+    speed = (Math.floor(speed) % 3) + 1;
+    document.getElementById(position).src =
+      "https://kyimages.kyinbridges.com/why-was-the-wood-from-sequoias-not-used-in-construction-.jpg";
+    position = position + speed;
+  } else {
+    alert("It's The Other Players Turn!");
+  }
+  if (position > 9) {
+    location.href = "win.html";
+  }
+  document.getElementById(position).src =
+    "https://static.vecteezy.com/system/resources/previews/001/193/929/large_2x/vintage-car-png.png";
+  checkAnswerExecuted = true;
 }
-function moveCar2(){
-    if(checkAnswerExecuted == true){
-        let speed2 = Math.random()*10
-        speed2 = Math.floor(speed2)%3 + 1
-        document.getElementById(position2).src = "https://www.roadsbridges.com/sites/rb/files/styles/content_type_page/public/Traffic_Safety_64.jpg?itok=a9CICO90"
-        position2 = position2 + speed2
-    }
-    else {
-        alert("It's The Other Players Turn!")
-    }
-    if(position2 > 28){
-        location.href = "win2.html"
-    }
-    document.getElementById(position2).src = "https://static.vecteezy.com/system/resources/previews/001/193/929/large_2x/vintage-car-png.png"
-    checkAnswerExecuted = false
+function moveCar2() {
+  if (checkAnswerExecuted == true) {
+    let speed2 = Math.random() * 10;
+    speed2 = (Math.floor(speed2) % 3) + 1;
+    document.getElementById(position2).src =
+      "https://kyimages.kyinbridges.com/why-was-the-wood-from-sequoias-not-used-in-construction-.jpg";
+    position2 = position2 + speed2;
+  } else {
+    alert("It's The Other Players Turn!");
+  }
+  if (position2 > 28) {
+    location.href = "win2.html";
+  }
+  document.getElementById(position2).src =
+    "https://static.vecteezy.com/system/resources/previews/001/193/929/large_2x/vintage-car-png.png";
+  checkAnswerExecuted = false;
 }
